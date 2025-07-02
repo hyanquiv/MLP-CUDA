b198a5dbe33b85ff569fed6bc2f2a4db90c97e7d
Henry Yanqui <hyanquiv@unsa.edu.pe>
2025-07-02 17:13:58 -0500

    prototipo v1.4

--- include/activation.h
+++ include/activation.h
@@ -1,8 +1,23 @@
 #ifndef ACTIVATION_H
 #define ACTIVATION_H
 
-__device__ float relu(float x);
-__device__ float relu_derivative(float x);
+#include <cuda_runtime.h>
+
+// Versión segura para inclusión múltiple
+#ifndef RELU_DEFINED
+#define RELU_DEFINED
+__device__ __forceinline__ float relu(float x)
+{
+    return x > 0 ? x : 0.0f;
+}
+
+__device__ __forceinline__ float relu_derivative(float x)
+{
+    return x > 0 ? 1.0f : 0.0f;
+}
+#endif
+
+// Kernels
 __global__ void softmax(float *input, float *output, int size);
 __global__ void relu_forward(float *input, float *output, int size);
 __global__ void relu_backward(float *d_output, float *input, float *d_input, int size);
